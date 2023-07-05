933b424a5478b0219a0b55c19c926d11f1958b75
Daniel mpm <dmarcosmpm.js@outlook.com>
2023-07-05 17:01:27 -0300

    → include @post & @get ←

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
   @Post() // Dados vindos de createUser DTO
-  async createUser(@Body() createUser: CreateUserDto)  { 
-    return {
-        ...createUser,
-        password: undefined
-    }
+  async createUser(@Body() createUser: CreateUserDto) {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser() {
+    return this.userService.getAllUser();
   }
 }
